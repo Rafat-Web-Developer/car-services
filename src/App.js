b78ea222bb115258c3bf11e150d2f983c21e22be
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-13 01:46:42 +0600

    Create two routes using react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 // import './App.css';
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 
@@ -6,6 +9,10 @@ function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
