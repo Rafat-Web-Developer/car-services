4d7695b8d9977a292c82ebd7514ff1401de3d21a
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-13 01:44:26 +0600

    Create some components and use header and footer components in app

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <small>Rafat Hossain</small>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
