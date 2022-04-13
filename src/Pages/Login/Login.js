4a0138d53cbac54a17f573d1fc16cf59bfc5ae14
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-13 12:49:12 +0600

    Design login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Component</h1>
-    </div>
+    <Card className="w-50 mx-auto my-5">
+      <Card.Header className="bg-primary fw-bold text-white fs-5">
+        Login
+      </Card.Header>
+      <Card.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="w-100">
+            Login
+          </Button>
+          <p className="my-3">
+            Create a new account{" "}
+            <Link
+              className="fw-bold text-primary"
+              style={{ textDecoration: "none" }}
+              to="/"
+            >
+              Sign up
+            </Link>
+          </p>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 };
 
