b3f96d91647607b1d59ef5ec27bd3798919d8a7f
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-15 00:38:22 +0600

    Sign in with google

--- src/Pages/Shared/SocialLink/SocialLink.js
+++ src/Pages/Shared/SocialLink/SocialLink.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const SocialLink = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from = location?.state?.from?.pathname || "/";
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  const handleGoogleSignIn = () => {
+    signInWithGoogle();
+  };
+  const handleGitHubSignIn = () => {};
+
   return (
     <div>
       <div className="mt-3 d-flex align-items-center justify-content-center">
@@ -8,10 +27,21 @@ const SocialLink = () => {
         <p className="text-primary mt-3 px-3 ">OR</p>
         <div style={{ height: "1px" }} className="bg-primary w-25"></div>
       </div>
+      {error && <p className="text-danger my-1">{error.message}</p>}
       <div className="my-3">
-        <button className="btn btn-success w-100 fw-bold mb-2">Google</button>
+        <button
+          className="btn btn-success w-100 fw-bold mb-2"
+          onClick={handleGoogleSignIn}
+        >
+          Google
+        </button>
         <button className="btn btn-primary w-100 fw-bold mb-2">Facebook</button>
-        <button className="btn btn-dark w-100 fw-bold mb-2">Github</button>
+        <button
+          className="btn btn-dark w-100 fw-bold mb-2"
+          onClick={handleGitHubSignIn}
+        >
+          Github
+        </button>
       </div>
     </div>
   );
